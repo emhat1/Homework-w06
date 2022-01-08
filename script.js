107ab72b4c897724828157c29d04285cd3ebe1db
Erin H <e.hatherell@gmail.com>
2022-01-08 19:03:42 +1100

    Previous searches work started - NEED EVENT LISTENER FOR BUTTON

--- script.js
+++ script.js
@@ -4,4 +4,6 @@
 var searchFormEl = document.querySelector("#locationSearchForm");
 var locationInputVal = document.querySelector("#inputLocation");
 var searchBtn = document.querySelector("#locationSearch");
-
+    // REMEMBER TO PUT IN EVENT LISTENER FOR SEARCH BUTTON!
+//Previous searches
+var usersLocationListGroupEl = document.querySelector(".prev-search");
