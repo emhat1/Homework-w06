a853e6997a46121f63ec9ce74ddcabeab29543ce
Erin H <e.hatherell@gmail.com>
2022-01-09 17:05:32 +1100

    Starting to set up forecast and setting up previous search list

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // JavaScript for Weather Dashboard using OpenWeather API
 
-// Search function
+// Defining variables
+// Search function variables
 var searchFormEl = document.querySelector("#locationSearchForm");
 var locationInputVal = document.querySelector("#inputLocation");
 var searchBtn = document.querySelector("#locationSearch");
-
-
-
-
+// Weather forecast variables
+var weatherContentDiv = document.querySelector("#weatherContent");
+var cardDivEl = document.querySelector(".card");
+var cardTitleEl = document.querySelector(".card-title");
+var weatherIconEl = document.querySelector("#icon");
+var uvIndexEl = document.querySelector("#uvIndex");
 //Previous searches
 var usersLocationListGroupEl = document.querySelector(".prev-search");
 
+// Loading the page
+window.onload = function initializeDashboard() {
+    // Generating locally saved location list
+    if (localStorage.getItem("location") !== null) {
+      for (var i = 0; i < existingEntries.length; i++) {
+        // Buttons for previous location listings
+        createNewLocationButton(existingEntries[i], usersLocationListGroupEl);
+     
+    }
 
+    
 // Add eventListener to search button
-searchBtn.addEventListener("click", // doublecheck command for here);
+searchBtn.addEventListener("click",handleSearch);
